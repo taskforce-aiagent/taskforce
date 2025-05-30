3982bd9950be68f29ce865dd187e3bfd45f03fc8
Aras Çağlıuzun <cagliuzun.aras@gmail.com>
2025-05-31 01:07:00 +0300

    github pages

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "TaskForce",
+  tagline: "…",
+  url: "https://taskforce-aiagent.github.io",
+  baseUrl: "/taskforce-aiagent/",
+  projectName: "taskforce-aiagent",
+  organizationName: "TaskForce",
+  deploymentBranch: "gh-pages",
+};
